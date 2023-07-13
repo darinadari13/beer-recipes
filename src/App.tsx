e91d48fa255698a982a39886b1f9ce5dcb51f368
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-07-13 22:54:10 +0300

    feat: add handleRightClick

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,54 @@
-import React, { useEffect } from 'react';
-import { useBeerStore } from './store/beerStore';
-
+import React, { useEffect } from "react";
+import { useBeerStore } from "./store/beerStore";
+import "./App.css";
 
 const App: React.FC = () => {
-  const beers = useBeerStore((state) => state.beers);
+  const beers = useBeerStore((state) => state.beers.slice(0, 15));
   const fetchBeers = useBeerStore((state) => state.fetchBeers);
-  // const loadMoreBeers = useBeerStore((state) => state.loadMoreBeers);
+  const loadMoreBeers = useBeerStore((state) => state.loadMoreBeers);
+  const toggleBeerSelection = useBeerStore((state) => state.toggleBeerSelection);
+  const selectedRecipes = useBeerStore((state) => state.selectedBeers);
+  const deleteSelectedBeers = useBeerStore((state) => state.deleteSelectedBeers);
 
   useEffect(() => {
     fetchBeers();
   }, [fetchBeers]);
+
+  useEffect(() => {
+    document.addEventListener("contextmenu", handleRightClick);
+    return () => {
+      document.removeEventListener("contextmenu", handleRightClick);
+    };
+  }, []);
+
+  const handleRightClick = (event: MouseEvent) => {
+    event.preventDefault();
+    const target = event.target as HTMLElement;
+    if (target.tagName === "LI") {
+      const recipeId = parseInt(target.dataset.id || "", 10);
+      if (recipeId) {
+        toggleBeerSelection(recipeId);
+      }
+    }
+  };
+
+  const handleDelete = () => {
+    deleteSelectedBeers();
+  };
 
   return (
     <div>
       <h1>Beer Recipes</h1>
       <ul>
         {beers.map((beer) => (
-          <li key={beer.id}>{beer.name}</li>
+          <li key={beer.id} data-id={beer.id} className={selectedRecipes.has(beer.id) ? "selected" : ""}>
+            {beer.name}
+          </li>
         ))}
       </ul>
-      {/* <button onClick={loadMoreBeers}>Load More</button> */}
+      {selectedRecipes.size > 0 && <button onClick={handleDelete}>Delete</button>}
     </div>
   );
 };
 
 export default App;
-
