140c8c107c4e9cc5187cd74b4888f047ce5203f4
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-07-13 17:57:30 +0300

    feat: rendered list of beers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import { useBeerStore } from './store/beerStore';
 
-function App() {
+
+const App: React.FC = () => {
+  const beers = useBeerStore((state) => state.beers);
+  const fetchBeers = useBeerStore((state) => state.fetchBeers);
+  // const loadMoreBeers = useBeerStore((state) => state.loadMoreBeers);
+
+  useEffect(() => {
+    fetchBeers();
+  }, [fetchBeers]);
+
   return (
-    <div className="App">
-      
+    <div>
+      <h1>Beer Recipes</h1>
+      <ul>
+        {beers.map((beer) => (
+          <li key={beer.id}>{beer.name}</li>
+        ))}
+      </ul>
+      {/* <button onClick={loadMoreBeers}>Load More</button> */}
     </div>
   );
-}
+};
 
 export default App;
+
---
+++ src/store/beerStore.ts
@@ -0,0 +1,61 @@
+import { create, } from 'zustand'
+import { fetchBeers } from "../services/fetchBeers/fetchBeers";
+import type { Beer } from "../services/fetchBeers/types";
+
+
+type BeerStore = {
+  beers: Beer[];
+  selectedBeers: Set<number>;
+  page: number;
+  fetchBeers: () => Promise<void>;
+  // loadMoreBeers: () => Promise<void>;
+  // toggleBeerSelection: (beerId: number) => void;
+  // deleteSelectedBeers: () => void;
+};
+
+export const useBeerStore = create<BeerStore>((set, get) => ({
+  beers:[],
+  selectedBeers: new Set<number>(),
+  page: 1,
+  fetchBeers: async () => {
+        const { page } = get();
+        const fetchedBeers = await fetchBeers(page);
+        set({ beers: fetchedBeers });
+      },
+}))
+
+
+
+// const beerStore = create<BeerStore>((set: SetState<BeerStore>, get: GetState<BeerStore>) => ({
+//   beers: [],
+//   selectedBeers: new Set<number>(),
+//   page: 1,
+//   fetchBeers: async () => {
+//     const { page } = get();
+//     const fetchedBeers = await fetchBeers(page);
+//     set({ beers: fetchedBeers });
+//   },
+//   loadMoreBeers: async () => {
+//     set((state) => ({ page: state.page + 1 }));
+//     await get().fetchBeers();
+//   },
+//   toggleBeerSelection: (beerId) => {
+//     set((state) => {
+//       const selectedBeers = new Set(state.selectedBeers);
+//       if (selectedBeers.has(beerId)) {
+//         selectedBeers.delete(beerId);
+//       } else {
+//         selectedBeers.add(beerId);
+//       }
+//       return { selectedBeers };
+//     });
+//   },
+//   deleteSelectedBeers: () => {
+//     set((state) => {
+//       const beers = state.beers.filter((beer) => !state.selectedBeers.has(beer.id));
+//       return { beers, selectedBeers: new Set<number>() };
+//     });
+//   },
+// }));
+
+
