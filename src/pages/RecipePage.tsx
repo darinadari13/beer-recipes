32895a3719b6c8f1cb22a3295292c9cd704d5f6b
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-07-14 20:06:54 +0300

    feat: install react virtualized

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -5,7 +5,7 @@ const RecipePage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const beerId = id ? parseInt(id) : 0;
   const beer = useBeerStore((state) => state.beers.find((beer) => beer.id === beerId));
-  console.log(beer);
+
   return (
     <div>
       <h1>{beer?.name}</h1>
