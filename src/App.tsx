5af58b3e1f2a047c2c44d7a7af0112ada35507bf
darinadari13 <92116950+darinadari13@users.noreply.github.com>
2023-07-19 11:24:03 +0300

    feat: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,45 @@
 import React, { useEffect } from "react";
 import { useBeerStore } from "./store/beerStore";
-import { useNavigate } from "react-router-dom";
-
+import { List } from "react-virtualized";
 import "./App.css";
+
+const Row: React.FC = ({ list, index, style }: any) => {
+  useEffect(() => {
+    console.log(list[index].name);
+
+    return () => {
+      console.log("unounted");
+    };
+  }, []);
+  return (
+    <div key={index} style={style}>
+      {list[index].name}
+    </div>
+  );
+};
 
 const App: React.FC = () => {
   const beers = useBeerStore((state) => state.beers.slice(0, 15));
   const fetchBeers = useBeerStore((state) => state.fetchBeers);
-  const toggleBeerSelection = useBeerStore((state) => state.toggleBeerSelection);
-  const selectedRecipes = useBeerStore((state) => state.selectedBeers);
-  const deleteSelectedBeers = useBeerStore((state) => state.deleteSelectedBeers);
-  const navigate = useNavigate();
 
   useEffect(() => {
     fetchBeers();
   }, [fetchBeers]);
 
-  useEffect(() => {
-    document.addEventListener("contextmenu", handleRightClick);
-    return () => {
-      document.removeEventListener("contextmenu", handleRightClick);
-    };
-  }, []);
-
-  const handleRightClick = (event: MouseEvent) => {
-    event.preventDefault();
-    const target = event.target as HTMLElement;
-    if (target.tagName === "LI") {
-      const recipeId = parseInt(target.dataset.id || "", 10);
-      if (recipeId) {
-        toggleBeerSelection(recipeId);
-      }
-    }
-  };
-
-  const handleDelete = () => {
-    deleteSelectedBeers();
-  };
-
-  const handleRecipeClick = (event: React.MouseEvent<HTMLLIElement>) => {
-    const target = event.target as HTMLLIElement;
-    const recipeId = parseInt(target.dataset.id || "", 10);
-    if (recipeId) {
-      navigate(`/recipes/${recipeId}`);
-    }
+  const rowRenderer = (props: any) => {
+    return <Row list={beers} key={props.index} {...props} />;
   };
 
   return (
-    <div>
-      <h1>Beer Recipes</h1>
-      <ul>
-        {beers.map((beer) => (
-          <li
-            key={beer.id}
-            data-id={beer.id}
-            className={selectedRecipes.has(beer.id) ? "selected" : ""}
-            onClick={handleRecipeClick}
-          >
-            {beer.name}
-          </li>
-        ))}
-      </ul>
-      {selectedRecipes.size > 0 && <button onClick={handleDelete}>Delete</button>}
+    <div style={{ border: "1px solid red" }}>
+      <List
+        width={300}
+        height={500}
+        rowCount={beers.length}
+        rowHeight={100}
+        rowRenderer={rowRenderer}
+        overscanRowCount={0}
+      />
     </div>
   );
 };
